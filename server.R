ca59c31e23a0f84bae4502866ecf8c8169b6cec1
FsGuerra <francisco.s.guerra@gmail.com>
2014-10-22 20:36:20 +0100

    Update server.R

--- server.R
+++ server.R
@@ -2,7 +2,9 @@ shinyServer(
 function(input, output) {
 output$oid1 <- renderPrint({input$id1})
 output$oid2 <- renderPrint({input$id2})
+output$oid3 <- renderPrint({input$id3})
+output$oid4 <- renderPrint({input$id1/input$id2})
 output$odate <- renderPrint({input$tdate-input$fdate})
-output$oaver <- renderPrint({input$id1*100/input$id2})
+output$oaver <- renderPrint({((input$id1/input$id2)*100)/input$id3})
 }
 )
